a88b6e20002dbd96e5232c1f1f28281371a1a031
Uwemski <iconuwemfrank@gmail.com>
2025-09-11 11:43:18 +0100

    playing with middleware because that's next, will delte the latest files soon

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(AdminMiddleware::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
