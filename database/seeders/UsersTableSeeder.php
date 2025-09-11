1ef9ff86f94dd3b3a4f3d7e278d235af9f2a7607
Uwemski <iconuwemfrank@gmail.com>
2025-09-11 15:45:59 +0100

    Added a seeder for the users table and also created middlewares for the 3 purposed users of the application

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        User::create([
+            'name'=> 'Admin',
+            'email'=> 'admin@test.com',
+            'password'=> bcrypt('password'),
+            'role'=> 'admin'
+        ]);
+    
+    }
+}
