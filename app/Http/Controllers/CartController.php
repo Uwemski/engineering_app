4c2d495e521ef73d7f3835e77416416f38224a1b
Uwemski <iconuwemfrank@gmail.com>
2025-10-17 10:33:00 +0100

    still working on the cart but added an event for when a new user registers.

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,7 +9,23 @@ class CartController extends Controller
 {
     //
     public function addToCart(Request $request, $id) {
-        $id = Product::findOrFail($id);
-        dd($id);
+        //find the id
+        $product = Product::findOrFail($id);
+
+        //use session helper
+        $cart = session('cart', []);
+
+         // Add or update the product in the cart
+        $cart[$id] = [
+            'name' => $product->name,
+            'quantity' => 1,
+            'price' => $product->price,
+            'image' => $product->image,
+            'description' => $product->description
+        ];
+        session()->put('cart', $cart);
+
+        return redirect()->back()->with('success', 'Product added successfully');//anything after return doesn't work
+        // dd($cart);
     }
 }
